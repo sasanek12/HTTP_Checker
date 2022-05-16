bd2a68e55c7d275042fcfd056b9cc05536804b04
sasanek12 <sasanek12@gmail.com>
2022-05-16 23:16:58 +0200

    a

--- main.py
+++ main.py
@@ -22,14 +22,17 @@ def main():
                 sys.exit(1)
             temp = rows[index].split()
             result = temp[0]
+            if int(result) > 1090 or int(result) < 870:
+                print("Ciśnienie przyjmuje niemożliwe wartości")
+                sys.exit(1)
             print("Ciśnienie w hPa na lotnisku w Balicach:")
             sys.stdout.write(result)
             sys.exit(0)
         else:
-            print("Strona nie zawiera przewidywanej zawartosci")
+            print("Strona nie zawiera przewidywanej zawartości")
             sys.exit(1)
     else:
-        print("Strona nie dziala poprawnie")
+        print("Strona nie działa poprawnie")
         sys.exit(1)
 
 
