59c851f57adf503b2748c6674ab1ee3fec472b89
sasanek12 <sasanek12@gmail.com>
2022-05-16 23:11:16 +0200

    a

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import sys
 import requests
+from bs4 import BeautifulSoup
 
 
 def main():
-    url = "http://th.if.uj.edu.pl/"
-    response_head = requests.head(url)
+    global cells, value
+    url = "https://www.burzochron.pl/meteo-data/metar/epkk/"
     response_content = requests.get(url)
-    if (response_head.headers["Content-Type"] == "text/html" and response_content.status_code == 200):
-        if (response_content.text.find("Institute of Theorethical Physics")):
-            print("Strona dziala poprawnie!")
+    if (response_content.headers["Content-Type"].find("text/html") != -1 and response_content.status_code == 200):
+        soup = BeautifulSoup(response_content.text, "lxml")
+        if (soup.find(class_='table-metar')):
+            headers = []
+            rows = []
+            for item in soup.find(class_='table-metar').find_all("th"):
+                headers.append(item.text)
+                rows.append(item.find_next_sibling().text)
+            try:
+                index = headers.index("Ciśnienie")
+            except ValueError:
+                print("Nie znaleziono ciśnienia w tabeli")
+                sys.exit(1)
+            temp = rows[index].split()
+            result = temp[0]
+            print("Ciśnienie w hPa na lotnisku w Balicach:")
+            sys.stdout.write(result)
             sys.exit(0)
-
-    print("Strona nie dziala poprawnie")
-    sys.exit(1)
+        else:
+            print("Strona nie zawiera przewidywanej zawartosci")
+            sys.exit(1)
+    else:
+        print("Strona nie dziala poprawnie")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
